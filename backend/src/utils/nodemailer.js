189002ccd27aeeba04808ad2c365d56cf370faf1
Nati-darse <natnaeldarsema@gmail.com>
2025-06-03 12:59:55 +0300

    Add Node 18 engine requirement

--- backend/src/utils/nodemailer.js
+++ backend/src/utils/nodemailer.js
@@ -4,7 +4,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host:"smtp-relay.brevo.com",
   port: 587,
-  service: 'Gmail', // or your SMTP provider
+    service: 'Gmail',// or your SMTP provider
   auth: {
     user: process.env.SENDER_EMAIL, // your Gmail address
     pass: process.env.SENDER_PASS  // the 16-character app password
