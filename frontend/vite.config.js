772365b02c211a83682010f728682b2a83b20f88
TD0961 <tensaedeme61@gmail.com>
2025-06-12 09:39:21 -0700

    resolving conflict in payment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    allowedHosts: [
-      "1d12-213-55-79-195.ngrok-free.app"
-    ]
-  }
 })
